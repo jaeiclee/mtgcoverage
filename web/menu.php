eb880511ebc3210b07d8d48923e9edfb941da2c9
ideocl4st <jaeic.lee@gmail.com>
2015-07-25 23:27:31 +0900

    no donations for now

--- web/menu.php
+++ web/menu.php
@@ -138,14 +138,14 @@ if ($datamagic['online'] == '1') {
                  </ul>
               </li>
 			              </ul>
-			  <ul class="nav navbar-nav navbar-right">
+			  <!--<ul class="nav navbar-nav navbar-right">
 				<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 				<input type="hidden" name="cmd" value="_s-xclick">
 				<input type="hidden" name="hosted_button_id" value="F2267LQ2XS2JC">
 				<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="Donation">
 				<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 				</form>
-			              </ul>
+			              </ul>-->
 
      
 		  
