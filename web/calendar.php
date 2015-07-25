0b90bde23d2ce616713a4a94f86083d1c87d8329
ideocl4st <jaeic.lee@gmail.com>
2015-07-25 18:43:13 +0900

    Menu for Calendar

--- web/calendar.php
+++ web/calendar.php
@@ -1,4 +1,3 @@
-<?php require 'config.php'; ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -17,12 +16,14 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <?php require 'config.php'; ?>
+  
+    <link href="css/navbar.css" rel="stylesheet">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+  
   </head>
   <body>
     <?php include 'menu.php'; ?>
-
-    <link href="css/navbar.css" rel="stylesheet">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
     
     <h1>Hello, world!</h1>
 
