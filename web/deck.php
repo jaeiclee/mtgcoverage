a13372e7941f0debbb6e3a704986750a6f26c33c
ideocl4st <jaeic.lee@gmail.com>
2015-09-28 19:51:40 +0900

    Betterer link detection

--- web/deck.php
+++ web/deck.php
@@ -23,8 +23,8 @@
   </head>
   <body>
     <?php include 'menu.php'; ?>
-	
-	<?php 
+
+	<?php
 	$mainquery = 'SELECT * FROM Decks WHERE ID ="' . $id . '"';
 
     try
@@ -42,21 +42,21 @@
 
     // Finally, we can retrieve all of the found rows into an array using fetchAll
     $rowdeck = $stmt->fetch();
-	
+
 	?>
-	
-	
+
+
 <div class="container">
 	<div class="jumbotron" style="padding-bottom: 20px">
 	<div class="row">
-	
+
 		<div class="col-md-4">
 			<div class="cliente" style="border: none;">
 				<h2><?php echo $rowdeck['Name']; ?></h2>
 				<br>
-				
-				
-				
+
+
+
 				<?
 				echo '<b>Format: </b>';
 				if ($rowdeck['Standard'] == 1) {
@@ -80,15 +80,15 @@
 				else {
 					echo 'Unknown';
 				}
-				
+
 				echo '<br>';
-				
+
 				if ($rowdeck['CBlock'] != null) {
 					echo '<b>Block: </b>';
 					echo $rowdeck['CBlock'];
 				}
 				?>
-				
+
 				<br>
 				<b>Info:</b><br><br>
 				<?php echo $rowdeck['Info']; ?>
@@ -100,15 +100,15 @@
 		<div class="col-md-8">
 			<div class="cliente" style="border: none;">
 				<h2>Games:</h2>
-	
-	<?php 
-	
-	
 
-	
-	$query = 'SELECT Matches.VOD, Matches.Format, Matches.PlayerIDA, Matches.PlayerIDB, Matches.DeckIDA, Matches.DeckIDB, Matches.TournamentID, Tournament.Name, Tournament.ID FROM Matches 
-			  INNER JOIN Tournament ON Matches.TournamentID=Tournament.ID 
-			  WHERE DeckIDA = "' . $id . '" OR DeckIDB = "' . $id . '" 
+	<?php
+
+
+
+
+	$query = 'SELECT Matches.VOD, Matches.Format, Matches.PlayerIDA, Matches.PlayerIDB, Matches.DeckIDA, Matches.DeckIDB, Matches.TournamentID, Tournament.Name, Tournament.ID FROM Matches
+			  INNER JOIN Tournament ON Matches.TournamentID=Tournament.ID
+			  WHERE DeckIDA = "' . $id . '" OR DeckIDB = "' . $id . '"
 			  ORDER BY Matches.Format DESC';
 
     try
@@ -127,10 +127,10 @@
     // Finally, we can retrieve all of the found rows into an array using fetchAll
     $rows = $stmt->fetchAll();
 
-	
+
 	// For each Row in Matches post that Row
-	
-	foreach($rows as $row): 
+
+	foreach($rows as $row):
 
 	// Declare values from Matches Table
 		$M_ID = $row['ID'];
@@ -142,15 +142,15 @@
 		$M_DeckIDB = $row['DeckIDB'];
 		$M_Format = $row['Format'];
 		$M_TournamentID = $row['TournamentID'];
-	
+
 		if (strpos($row['VOD'], 'youtu') !== false) { echo '<img src="images/youtube.png"> '; }
 		elseif (strpos($row['VOD'], 'twitch') !== false) { echo '<img src="images/twitch.png"> '; }
 		echo '<a href=" ' .htmlentities($row["VOD"], ENT_QUOTES,"UTF-8"). '" target="_newtab" ">VOD</a>';
-		
+
 
 			// GET Player A
-			
-			
+
+
 			$queryplayersA = 'SELECT ID, Name FROM Players WHERE ID = "' . $M_PlayerIDA . '"' ;
 			try
 			{
@@ -167,24 +167,24 @@
 
 			// Finally, we can retrieve all of the found rows into an array using fetchAll
 			$rows3A = $stmt3A->fetchAll();
-	
+
 			// For each Row in Matches that matches TournamentID post those rows
 			foreach($rows3A as $row3A):
-			
-			// Declare values from Tournament Table	
+
+			// Declare values from Tournament Table
 			$P_IDA = $row3A['ID'];
 			$P_NameA = $row3A['Name'];
 			?>
-			
+
 			<a href="player.php?id=<?php echo $P_IDA; ?>"><?php echo $P_NameA; ?></a>
-			
-			<?php endforeach; ?>  
-			
+
+			<?php endforeach; ?>
+
 			<?php
-			
+
 			// GET Deck A
-			
-			
+
+
 			$querydecksA = 'SELECT ID, Name FROM Decks WHERE ID = "' . $M_DeckIDA . '"' ;
 			try
 			{
@@ -201,32 +201,32 @@
 
 			// Finally, we can retrieve all of the found rows into an array using fetchAll
 			$rows4A = $stmt4A->fetchAll();
-	
+
 			// For each Row in Matches that matches TournamentID post those rows
 			foreach($rows4A as $row4A):
-			
-			// Declare values from Tournament Table	
+
+			// Declare values from Tournament Table
 			$D_IDA = $row4A['ID'];
 			$D_NameA = $row4A['Name'];
-			
+
 			echo ' (<a href=deck.php?id=' . $D_IDA . '>' . $D_NameA . '</a>)';
 			?>
-			
-			<?php endforeach; ?>  
-			
-			
-			
-			<?php echo " VS "; ?>
-			
-			
-			
-			<?php 
-			
 
-			
-			
+			<?php endforeach; ?>
+
+
+
+			<?php echo " VS "; ?>
+
+
+
+			<?php
+
+
+
+
 			// GET Player B
-			
+
 			$queryplayersB = 'SELECT ID, Name FROM Players WHERE ID = "' . $M_PlayerIDB . '"';
 			try
 			{
@@ -243,23 +243,23 @@
 
 			// Finally, we can retrieve all of the found rows into an array using fetchAll
 			$rows3B = $stmt3B->fetchAll();
-	
+
 			// For each Row in Matches that matches TournamentID post those rows
 			foreach($rows3B as $row3B):
-			
-			// Declare values from Tournament Table	
+
+			// Declare values from Tournament Table
 			$P_IDB = $row3B['ID'];
 			$P_NameB = $row3B['Name'];
 			?>
-			
-		
+
+
 			<a href="player.php?id=<?php echo $P_IDB; ?>"><?php echo $P_NameB; ?></a>
-			<?php endforeach; ?>  
-			
+			<?php endforeach; ?>
+
 			<?php
-			
+
 			// GET Deck B
-			
+
 			$querydecksB = 'SELECT ID, Name FROM Decks WHERE ID = "' . $M_DeckIDB . '"' ;
 			try
 			{
@@ -276,34 +276,34 @@
 
 			// Finally, we can retrieve all of the found rows into an array using fetchAll
 			$rows4B = $stmt4B->fetchAll();
-	
+
 			// For each Row in Matches that matches TournamentID post those rows
 			foreach($rows4B as $row4B):
-			
-			// Declare values from Tournament Table	
+
+			// Declare values from Tournament Table
 			$D_IDB = $row4B['ID'];
 			$D_NameB = $row4B['Name'];
-			
+
 			echo ' (<a href=deck.php?id=' . $D_IDB . '>' . $D_NameB . '</a>)';
 			?>
 
-			<?php endforeach; ?>  
+			<?php endforeach; ?>
 		<br>
-		
-	
-	<?php endforeach; ?> 
-	
-	
-	
-	
-	
-	
-	
-	
-	<?php 
-	
+
+
+	<?php endforeach; ?>
+
+
+
+
+
+
+
+
+	<?php
+
 	// GET Decktechs
-	
+
 	$decktechquery = 'SELECT * FROM Decktechs WHERE DeckID ="' . $id . '"';
 
     try
@@ -321,12 +321,12 @@
 
     // Finally, we can retrieve all of the found rows into an array using fetchAll
     $rowsdecktechs = $stmt->fetchAll();
-	
+
 	if (count($rowsdecktechs) >= 1) {
 	echo'<h2>Decktechs:</h2>';
 	}
-	
-	foreach($rowsdecktechs as $rowdecktech): 
+
+	foreach($rowsdecktechs as $rowdecktech):
 
 	// Declare values from Matches Table
 		$DT_ID = $rowdecktech['ID'];
@@ -334,15 +334,15 @@
 		$DT_PlayerID = $rowdecktech['PlayerID'];
 		$DT_DeckID = $rowdecktech['DeckID'];
 		$DT_TournamentID = $rowdecktech['TournamentID'];
-	
-		if (strpos($DT_VOD, 'youtube') !== false) { echo '<img src="images/youtube.png"> '; }
+
+		if (strpos($DT_VOD, 'youtu') !== false) { echo '<img src="images/youtube.png"> '; }
 		elseif (strpos($DT_VOD, 'twitch') !== false) { echo '<img src="images/twitch.png"> '; }
 		echo '<a href=" ' .htmlentities($DT_VOD, ENT_QUOTES,"UTF-8"). '" target="_newtab" ">VOD</a> ';
-	
-	
+
+
 		// GET Player Name if available
 		if ($DT_PlayerID != '0') {
-			
+
 		$DTplayerquery = 'SELECT ID, Name FROM Players WHERE ID ="' . $DT_PlayerID . '"';
 
 		try
@@ -360,14 +360,14 @@
 
 		// Finally, we can retrieve all of the found rows into an array using fetchAll
 		$DTplayer = $stmt->fetch();
-	
+
 		echo "by " . $DTplayer['Name'] . "";
 		}
-		
-		
+
+
 		// Get Tournament Name if available
 		if ($DT_TournamentID != '0') {
-			
+
 		$DTtournamentquery = 'SELECT ID, Name FROM Tournament WHERE ID ="' . $DT_TournamentID . '"';
 
 		try
@@ -385,15 +385,15 @@
 
 		// Finally, we can retrieve all of the found rows into an array using fetchAll
 		$DTtournament = $stmt->fetch();
-	
+
 		echo " at " . $DTtournament['Name'] . "<br>";
 		}
-		
-		
+
+
 		endforeach;
 	?>
-	
-	
+
+
 			</div>
 		</div>
 	</div>
